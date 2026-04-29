0b5d30d38b2ce991f3bbed290a6a085991a9b191
Phuong Quang Nguyen <phnguyen@Phuongs-MacBook-Pro.local>
2026-04-29 17:19:53 +0200

    Redesign restaurant homepage and menu

--- backend/restaurant_site/urls.py
+++ backend/restaurant_site/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
     path('', include('api.urls')),
 ]
 
